02bc192e64498eb07b4438777c8bf1029c7c1574
Brandon <morenob2525@gmail.com>
2021-03-05 01:07:06 -0600

    cambios de diseño del form en cliente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EntrarComponent } from '../app/componentes/entrar/entrar.component';
 import { PrincipalComponent } from '../app/componentes/principal/principal.component';
+import {AgregarClienteComponent} from '../app/componentes/agregar-cliente/agregar-cliente.component';
+
 const routes: Routes = [
   { path: 'entrar', component: EntrarComponent},
   {path: 'principal', component: PrincipalComponent},
+  {path: 'AgregarCliente', component: AgregarClienteComponent},
 
 ];
 
